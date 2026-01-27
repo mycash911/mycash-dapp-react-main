69f4638947318b32acbfe62bd6a4255466041e43
mycash <inambabu030@gmail.com>
2026-01-27 13:18:15 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ import './appkit';
 // Just render the React app normally
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
